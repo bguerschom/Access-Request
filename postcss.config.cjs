04e82d9efec23c4a0fc61a7821555f1eb83eeb41
bguerschom <148221717+bguerschom@users.noreply.github.com>
2024-12-03 09:20:27 +0200

    Update postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,9 @@
-// postcss.config.cjs
-export default {
+/** @type {import('postcss-load-config').Config} */
+const config = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
-  },
+  }
 }
+
+export default config
