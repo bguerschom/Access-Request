ccfb8c8a530b6ba7cfd84864e126a550f6d0992f
bguerschom <148221717+bguerschom@users.noreply.github.com>
2024-12-04 22:33:25 +0200

    Update UploadPage.jsx

--- src/pages/UploadPage.jsx
+++ src/pages/UploadPage.jsx
@@ -462,6 +462,8 @@ const handleReset = () => {
       </Card>
     </div>
 
+      )
+
     <footer className="bg-[#0A2647] text-white py-4 mt-auto">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center">
