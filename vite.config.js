f49f7089da7318612abdd6c44fb0172acd029e17
bguerschom <148221717+bguerschom@users.noreply.github.com>
2024-12-05 17:55:24 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -9,13 +9,16 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src')
     }
   },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          pdfjsLib: ['pdfjs-dist']
-        }
+build: {
+  rollupOptions: {
+    output: {
+      globals: {
+        recharts: 'Recharts'
+      },
+      manualChunks: {
+        pdfjsLib: ['pdfjs-dist']
       }
     }
   }
+}
 })
