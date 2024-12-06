ef0b473da2ce3e88715927c75ef0fe3270ee13b2
bguerschom <148221717+bguerschom@users.noreply.github.com>
2024-12-06 11:09:07 +0200

    Update useAuth.js

--- src/hooks/useAuth.js
+++ src/hooks/useAuth.js
@@ -1,20 +1,32 @@
 // src/hooks/useAuth.js
 import { useState, useEffect } from 'react';
-import { auth } from '@/config/firebase';
-import { onAuthStateChanged } from 'firebase/auth';
+import { auth, db } from '@/config/firebase';
+import { doc, getDoc } from 'firebase/firestore';
 
 export const useAuth = () => {
   const [user, setUser] = useState(null);
+  const [userData, setUserData] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    const unsubscribe = onAuthStateChanged(auth, (user) => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
       setUser(user);
+      
+      if (user) {
+        // Get user data from Firestore
+        const userDoc = await getDoc(doc(db, 'users', user.uid));
+        if (userDoc.exists()) {
+          setUserData(userDoc.data());
+        }
+      } else {
+        setUserData(null);
+      }
+      
       setLoading(false);
     });
 
     return () => unsubscribe();
   }, []);
 
-  return { user, loading };
+  return { user, userData, loading };
 };
